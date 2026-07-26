dd3f9c9a1de5a3925d88e6f93731463bb34a2cd8
sanyzrn <99163321+sanyzrn@users.noreply.github.com>
2026-07-26 07:28:31 +0330

    chore: remove React/Vite prototype contamination (#10)
    Delete root nex-prototype shell and react-prototype CI job. Flutter/Dart client only.

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { NexApp } from "./app/NexApp";
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <NexApp />
-  </StrictMode>,
-);
